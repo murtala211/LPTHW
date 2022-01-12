dabe6e059aa7aea1f629b268eb16c40dc59d6117
Murtala Aminu B <murtala211@yahoo.com>
2022-01-12 23:01:33 +0000

    Finished Exercise 6

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -13,3 +13,10 @@ print(f"I also said: '{y}'")
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w+e)
