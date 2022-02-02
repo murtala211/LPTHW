98c98d8096c87707cf0bfab6ad75d211ee04fdbc
Murtala Aminu B <murtala211@yahoo.com>
2022-02-02 21:57:21 +0000

    Updated missing lines 18-23

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -15,3 +15,9 @@ print("What is 3 + 2?", 3 +2)
 print("what is 5 - 7?", 5 - 7)
 
 print("Oh, that's why it's false.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5>= -2)
+print("Is it less or equal?", 5 <= -2)
